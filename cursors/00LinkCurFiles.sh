23b6a6f8dee5d4298641ddea3d2e2b294331fd7c
ooxxvv <ooxxvv@gmail.com>
2018-08-19 13:39:03 +0800

    Make cursor more simple

--- cursors/00LinkCurFiles.sh
+++ cursors/00LinkCurFiles.sh
@@ -4,12 +4,15 @@
 # Set working directory as the directory of this script
 cd "$(dirname "$0")"
 
-################################################### Busy & Background #
+############################################################ ani-busy #
+###### wait
 ln -sf curFiles/ani-busy.cur wait
 ln -sf curFiles/ani-busy.cur watch
 ln -sf curFiles/ani-busy.cur clock
 ln -sf curFiles/ani-busy.cur 0426c94ea35c87780ff01dc239897213
 
+###################################################### ani-background #
+###### half-busy
 ln -sf curFiles/ani-background.cur half-busy
 ln -sf curFiles/ani-background.cur progress
 ln -sf curFiles/ani-background.cur left_ptr_watch
@@ -18,164 +21,240 @@ ln -sf curFiles/ani-background.cur 08e8e1c95fe2fc01f976f1e063a24ccd
 ln -sf curFiles/ani-background.cur 3ecb610c1bf2410f44200f48c40d3599
 ln -sf curFiles/ani-background.cur 9116a3ea924ed2162ecab71ba103b17f
 
-###################################################### Normal Pointer #
+########################################################### a-pointer #
+###### default mouse cursor pointing left
 ln -sf curFiles/a-pointer.cur default
 ln -sf curFiles/a-pointer.cur left_ptr
 ln -sf curFiles/a-pointer.cur top_left_arrow
 ln -sf curFiles/a-pointer.cur left-arrow
 
-########################################################## Click-able #
-ln -sf curFiles/click-able.cur link
-ln -sf curFiles/click-able.cur alias
-ln -sf curFiles/click-able.cur 0876e1c15ff2fc01f906f1c363074c0f
-ln -sf curFiles/click-able.cur 3085a0e285430894940527032f8b26df
-ln -sf curFiles/click-able.cur 640fb0e74195791501fd1ed57b41487f
-ln -sf curFiles/click-able.cur a2a266d0498c3104214a47bd64ab0fc8
-ln -sf curFiles/click-able.cur dnd-link
-ln -sf curFiles/click-able.cur dnd-move
-ln -sf curFiles/click-able.cur dnd-ask
+########################################################## click-able #
+###### pointer (hand)
 ln -sf curFiles/click-able.cur pointer
 ln -sf curFiles/click-able.cur pointing_hand
 ln -sf curFiles/click-able.cur hand
 ln -sf curFiles/click-able.cur hand1
 ln -sf curFiles/click-able.cur e29285e634086352946a0e7090d73106
+###### alternative pointer
 ln -sf curFiles/click-able.cur hand2
+###### help
 ln -sf curFiles/click-able.cur help
 ln -sf curFiles/click-able.cur question_arrow
 ln -sf curFiles/click-able.cur whats_this
 ln -sf curFiles/click-able.cur gumby
 ln -sf curFiles/click-able.cur 5c6cd98b3f3ebcb1f9c7f1c204630408
 ln -sf curFiles/click-able.cur d9ce0ab605698f320427677b458ad60b
+###### create link/shortcut
+ln -sf curFiles/click-able.cur link
+ln -sf curFiles/click-able.cur alias
+ln -sf curFiles/click-able.cur 0876e1c15ff2fc01f906f1c363074c0f
+ln -sf curFiles/click-able.cur 3085a0e285430894940527032f8b26df
+ln -sf curFiles/click-able.cur 640fb0e74195791501fd1ed57b41487f
+ln -sf curFiles/click-able.cur a2a266d0498c3104214a47bd64ab0fc8
 
-########################################################### Forbidden #
+########################################################### drag-able #
+###### drag-and-drop to create link and other functions
+ln -sf curFiles/drag-able.cur dnd-link
+ln -sf curFiles/drag-able.cur dnd-ask
+ln -sf curFiles/drag-able.cur dnd-move
+ln -sf curFiles/drag-able.cur dnd-none
+ln -sf curFiles/drag-able.cur dnd-copy
+###### copy
+ln -sf curFiles/drag-able.cur copy
+ln -sf curFiles/drag-able.cur 08ffe1cb5fe6fc01f906f1c063814ccf
+ln -sf curFiles/drag-able.cur 1081e37283d90000800003c07f3ef6bf
+ln -sf curFiles/drag-able.cur 6407b0e94181790501fd1e167b474872
+ln -sf curFiles/drag-able.cur b66166c04f8c3109214a4fbd64a50fc8
+
+################################################################ text #
+###### text: I-beam
+ln -sf curFiles/text.cur text
+ln -sf curFiles/text.cur ibeam
+ln -sf curFiles/text.cur xterm
+###### text: vertical I-beam
+ln -sf curFiles/text.cur vertical-text
+ln -sf curFiles/text.cur 048008013003cff3c00c801001200000
+###### text: crosshair	crosshair
+ln -sf curFiles/text.cur crosshair
+###### pencil
+ln -sf curFiles/text.cur pencil
+###### cursor pointing right
+ln -sf curFiles/text.cur arrow
+ln -sf curFiles/text.cur right_ptr
+ln -sf curFiles/text.cur top_right_arrow
+ln -sf curFiles/text.cur right-arrow
+
+########################################################### forbidden #
+###### forbidden
 ln -sf curFiles/forbidden.cur forbidden
 ln -sf curFiles/forbidden.cur not-allowed
 ln -sf curFiles/forbidden.cur crossed_circle
 ln -sf curFiles/forbidden.cur 03b6e0fcb3499374a867c041f52298f0
 ln -sf curFiles/forbidden.cur circle
+###### drag-and-drop forbidden
 ln -sf curFiles/forbidden.cur dnd-no-drop
 ln -sf curFiles/forbidden.cur no-drop
 ln -sf curFiles/forbidden.cur 03b6e0fcb3499374a867c041f52298f0
 ln -sf curFiles/forbidden.cur 03b6e0fcb3499374a867d041f52298f0
+###### pirate/kill
 ln -sf curFiles/forbidden.cur pirate
 ln -sf curFiles/forbidden.cur kill
 
-######################################################### Hand writer #
-ln -sf curFiles/hand-writer.cur pencil
 
-########################################################## Input-Text #
-ln -sf curFiles/input-text.cur text
-ln -sf curFiles/input-text.cur arrow
-ln -sf curFiles/input-text.cur right_ptr
-ln -sf curFiles/input-text.cur ibeam
-ln -sf curFiles/input-text.cur xterm
-ln -sf curFiles/input-text.cur vertical-text
-ln -sf curFiles/input-text.cur 048008013003cff3c00c801001200000
-ln -sf curFiles/input-text.cur crosshair
 
-########################################################### Move-able #
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+########################################################### move-able #
+###### resize all
 ln -sf curFiles/move-able.cur size_all
+###### move
 ln -sf curFiles/move-able.cur move
 ln -sf curFiles/move-able.cur fleur
 ln -sf curFiles/move-able.cur 4498f0e0c1937ffe01fd06f973665830
 ln -sf curFiles/move-able.cur 9081237383d90e509aa00f00170e968f
 ln -sf curFiles/move-able.cur fcf21c00b30f7e3f83fe0dfd12e71cff
+###### scroll in all directions
 ln -sf curFiles/move-able.cur all-scroll
+###### grabbed
+ln -sf curFiles/move-able.cur closedhand
+ln -sf curFiles/move-able.cur grabbing
+ln -sf curFiles/move-able.cur 208530c400c041818281048008011002
+###### can grab
+ln -sf curFiles/move-able.cur openhand
+ln -sf curFiles/move-able.cur 5aca4d189052212118709018842178c0
+ln -sf curFiles/move-able.cur 9d800788f1b08800ae810202380a0822
 
-############################################################# Selctor #
-ln -sf curFiles/selector.cur copy
-ln -sf curFiles/selector.cur 08ffe1cb5fe6fc01f906f1c063814ccf
-ln -sf curFiles/selector.cur 1081e37283d90000800003c07f3ef6bf
-ln -sf curFiles/selector.cur 6407b0e94181790501fd1e167b474872
-ln -sf curFiles/selector.cur b66166c04f8c3109214a4fbd64a50fc8
-ln -sf curFiles/selector.cur dnd-copy
-ln -sf curFiles/selector.cur plus
-ln -sf curFiles/selector.cur cross
-ln -sf curFiles/selector.cur diamond_cross
-ln -sf curFiles/selector.cur target
-ln -sf curFiles/selector.cur cell
-ln -sf curFiles/selector.cur X_cursor
-ln -sf curFiles/selector.cur X-cursor
-
-############################################################### Sizer #
-ln -sf curFiles/sizer-H.cur ew-resize
-ln -sf curFiles/sizer-H.cur size_hor
-ln -sf curFiles/sizer-H.cur h_double_arrow
-ln -sf curFiles/sizer-H.cur 028006030e0e7ebffc7f7070c0600140
-ln -sf curFiles/sizer-H.cur e-resize
-ln -sf curFiles/sizer-H.cur right_side
-ln -sf curFiles/sizer-H.cur w-resize
-ln -sf curFiles/sizer-H.cur left_side
-ln -sf curFiles/sizer-H.cur col-resize
-ln -sf curFiles/sizer-H.cur sb_h_double_arrow
-ln -sf curFiles/sizer-H.cur 043a9f68147c53184671403ffa811cc5
-ln -sf curFiles/sizer-H.cur 14fef782d02440884392942c11205230
-ln -sf curFiles/sizer-H.cur split_h
-ln -sf curFiles/sizer-H.cur right_tee
-ln -sf curFiles/sizer-H.cur left_tee
-
-ln -sf curFiles/sizer-LR.cur nw-resize
-ln -sf curFiles/sizer-LR.cur top_left_corner
-ln -sf curFiles/sizer-LR.cur se-resize
-ln -sf curFiles/sizer-LR.cur bottom_right_corner
-ln -sf curFiles/sizer-LR.cur size_fdiag
-ln -sf curFiles/sizer-LR.cur nwse-resize
-ln -sf curFiles/sizer-LR.cur 38c5dff7c7b8962045400281044508d2
-ln -sf curFiles/sizer-LR.cur c7088f0f3e6c8088236ef8e1e3e70000
-
-ln -sf curFiles/sizer-RL.cur ne-resize
-ln -sf curFiles/sizer-RL.cur top_right_corner
-ln -sf curFiles/sizer-RL.cur sw-resize
-ln -sf curFiles/sizer-RL.cur bottom_left_corner
-ln -sf curFiles/sizer-RL.cur size_bdiag
-ln -sf curFiles/sizer-RL.cur nesw-resize
-ln -sf curFiles/sizer-RL.cur 50585d75b494802d0151028115016902
-ln -sf curFiles/sizer-RL.cur fcf1c3c7cd4491d801f1e1c78f100000
-
+############################################################# sizer-V #
+###### resize NS |
 ln -sf curFiles/sizer-V.cur ns-resize
 ln -sf curFiles/sizer-V.cur size_ver
 ln -sf curFiles/sizer-V.cur v_double_arrow
 ln -sf curFiles/sizer-V.cur double_arrow
 ln -sf curFiles/sizer-V.cur 00008160000006810000408080010102
+###### resize N /\
 ln -sf curFiles/sizer-V.cur n-resize
 ln -sf curFiles/sizer-V.cur top_side
+###### resize S \/
 ln -sf curFiles/sizer-V.cur s-resize
 ln -sf curFiles/sizer-V.cur bottom_side
+###### resize row
 ln -sf curFiles/sizer-V.cur row-resize
 ln -sf curFiles/sizer-V.cur sb_v_double_arrow
 ln -sf curFiles/sizer-V.cur 2870a09082c103050810ffdffffe0204
 ln -sf curFiles/sizer-V.cur c07385c7190e701020ff7ffffd08103c
 ln -sf curFiles/sizer-V.cur split_v
+
+###### TK arrow
 ln -sf curFiles/sizer-V.cur bottom_tee
 ln -sf curFiles/sizer-V.cur top_tee
 
-###################################################### Something else #
-ln -sf curFiles/something-else.cur right-arrow
-ln -sf curFiles/something-else.cur top_right_arrow
-ln -sf curFiles/something-else.cur closedhand
-ln -sf curFiles/something-else.cur grabbing
-ln -sf curFiles/something-else.cur 208530c400c041818281048008011002
-ln -sf curFiles/something-else.cur dnd-none
-ln -sf curFiles/something-else.cur openhand
-ln -sf curFiles/something-else.cur 5aca4d189052212118709018842178c0
-ln -sf curFiles/something-else.cur 9d800788f1b08800ae810202380a0822
-ln -sf curFiles/something-else.cur context-menu
-ln -sf curFiles/something-else.cur 08ffe1e65f80fcfdf9fff11263e74c48
-ln -sf curFiles/something-else.cur center_ptr
-ln -sf curFiles/something-else.cur up_arrow
-ln -sf curFiles/something-else.cur sb_up_arrow
-ln -sf curFiles/something-else.cur sb_down_arrow
-ln -sf curFiles/something-else.cur sb_right_arrow
-ln -sf curFiles/something-else.cur sb_left_arrow
-ln -sf curFiles/something-else.cur color-picker
+############################################################# sizer-H #
+###### resize EW -
+ln -sf curFiles/sizer-H.cur ew-resize
+ln -sf curFiles/sizer-H.cur size_hor
+ln -sf curFiles/sizer-H.cur h_double_arrow
+ln -sf curFiles/sizer-H.cur 028006030e0e7ebffc7f7070c0600140
+###### resize E >
+ln -sf curFiles/sizer-H.cur e-resize
+ln -sf curFiles/sizer-H.cur right_side
+###### resize W <
+ln -sf curFiles/sizer-H.cur w-resize
+ln -sf curFiles/sizer-H.cur left_side
+###### resize column
+ln -sf curFiles/sizer-H.cur col-resize
+ln -sf curFiles/sizer-H.cur sb_h_double_arrow
+ln -sf curFiles/sizer-H.cur 043a9f68147c53184671403ffa811cc5
+ln -sf curFiles/sizer-H.cur 14fef782d02440884392942c11205230
+ln -sf curFiles/sizer-H.cur split_h
 
-########################################################### Zoom-able #
+###### TK arrow
+ln -sf curFiles/sizer-H.cur right_tee
+ln -sf curFiles/sizer-H.cur left_tee
+
+############################################################ sizer-LR #
+###### resize NW \
+ln -sf curFiles/sizer-LR.cur nw-resize
+ln -sf curFiles/sizer-LR.cur top_left_corner
+###### resize SE \
+ln -sf curFiles/sizer-LR.cur se-resize
+ln -sf curFiles/sizer-LR.cur bottom_right_corner
+###### resize diag \
+ln -sf curFiles/sizer-LR.cur size_fdiag
+ln -sf curFiles/sizer-LR.cur nwse-resize
+ln -sf curFiles/sizer-LR.cur 38c5dff7c7b8962045400281044508d2
+ln -sf curFiles/sizer-LR.cur c7088f0f3e6c8088236ef8e1e3e70000
+
+############################################################ sizer-RL #
+###### resize NE /
+ln -sf curFiles/sizer-RL.cur ne-resize
+ln -sf curFiles/sizer-RL.cur top_right_corner
+###### resize SW /
+ln -sf curFiles/sizer-RL.cur sw-resize
+ln -sf curFiles/sizer-RL.cur bottom_left_corner
+###### resize diag /
+ln -sf curFiles/sizer-RL.cur size_bdiag
+ln -sf curFiles/sizer-RL.cur nesw-resize
+ln -sf curFiles/sizer-RL.cur 50585d75b494802d0151028115016902
+ln -sf curFiles/sizer-RL.cur fcf1c3c7cd4491d801f1e1c78f100000
+
+########################################################### zoom-able #
+###### zoom
 ln -sf curFiles/zoom-able.cur zoom
-ln -sf curFiles/zoom-able.cur f41c0e382c94c0958e07017e42b00462
+###### zoom in
 ln -sf curFiles/zoom-able.cur zoom-in
 ln -sf curFiles/zoom-able.cur zoom_in
 ln -sf curFiles/zoom-able.cur f41c0e382c94c0958e07017e42b00462
+###### zoom out
 ln -sf curFiles/zoom-able.cur zoom-out
 ln -sf curFiles/zoom-able.cur zoom_out
 ln -sf curFiles/zoom-able.cur f41c0e382c97c0938e07017e42800402
+
+###################################################### something-else #
+###### cursor pointing up
+ln -sf curFiles/something-else.cur center_ptr
+###### up arrow
+ln -sf curFiles/something-else.cur up_arrow
+###### color picker
+ln -sf curFiles/something-else.cur color-picker
+###### cross - target cursor
+ln -sf curFiles/something-else.cur cross
+ln -sf curFiles/something-else.cur diamond_cross
+ln -sf curFiles/something-else.cur target
+###### cell target
+ln -sf curFiles/something-else.cur cell
+###### plus icon
+ln -sf curFiles/something-else.cur plus
+###### X11 icon
+ln -sf curFiles/something-else X_cursor
+ln -sf curFiles/something-else X-cursor
+###### cursor with context menu emblem
+ln -sf curFiles/something-else.cur context-menu
+ln -sf curFiles/something-else.cur 08ffe1e65f80fcfdf9fff11263e74c48
+###### TK arrow
+ln -sf curFiles/sizer-H.cur sb_right_arrow
+ln -sf curFiles/sizer-H.cur sb_left_arrow
+ln -sf curFiles/sizer-V.cur sb_up_arrow
+ln -sf curFiles/sizer-V.cur sb_down_arrow
 
--- source/config/00BuildCurFiles.sh
+++ source/config/00BuildCurFiles.sh
@@ -11,15 +11,14 @@ xcursorgen ani-busy.config       ../../cursors/curFiles/ani-busy.cur
 ################################################################# CUR #
 xcursorgen a-pointer.config      ../../cursors/curFiles/a-pointer.cur
 xcursorgen click-able.config     ../../cursors/curFiles/click-able.cur
+xcursorgen drag-able.config      ../../cursors/curFiles/drag-able.cur
 xcursorgen forbidden.config      ../../cursors/curFiles/forbidden.cur
-xcursorgen hand-writer.config    ../../cursors/curFiles/hand-writer.cur
-xcursorgen input-text.config     ../../cursors/curFiles/input-text.cur
 xcursorgen move-able.config      ../../cursors/curFiles/move-able.cur
-xcursorgen selector.config       ../../cursors/curFiles/selector.cur
 xcursorgen sizer-H.config        ../../cursors/curFiles/sizer-H.cur
 xcursorgen sizer-LR.config       ../../cursors/curFiles/sizer-LR.cur
 xcursorgen sizer-RL.config       ../../cursors/curFiles/sizer-RL.cur
 xcursorgen sizer-V.config        ../../cursors/curFiles/sizer-V.cur
 xcursorgen something-else.config ../../cursors/curFiles/something-else.cur
+xcursorgen text.config           ../../cursors/curFiles/text.cur
 xcursorgen zoom-able.config      ../../cursors/curFiles/zoom-able.cur
 
